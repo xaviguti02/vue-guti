0adbb7276da0d090977910844724d54fe84effc1
Xavi Guti <xaviergutierrez@iesebre.com>
2025-05-08 12:06:18 +0200

    a

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -1,23 +1,21 @@
 // netlify/functions/api-proxy.js
-const fetch = require("node-fetch");
 
-exports.handler = async function (event, context) {
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
   try {
-    const response = await fetch("https://myheroacademia-api.onrender.com/personatges");
+    const response = await fetch('https://myheroacademia-api.onrender.com/personatges');
     const data = await response.json();
 
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*", // Evita problemes CORS
-        "Content-Type": "application/json",
-      },
       body: JSON.stringify(data),
     };
   } catch (error) {
+    console.error('Error fent la crida a l’API:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Error carregant dades de l'API" }),
+      body: JSON.stringify({ error: 'Error carregant dades' }),
     };
   }
 };
